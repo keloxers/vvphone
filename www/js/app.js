918f6153bf2e7897fde11e18b78afa423677f66b
Miguel Mendez <keloxers@gmail.com>
2014-04-07 19:21:20 -0300

    update

--- www/js/app.js
+++ www/js/app.js
@@ -2,22 +2,40 @@ angular.module('todo', ['ionic'])
 
 .controller('TodoCtrl', function($scope, $http) {
 
-  $http.get('http://www.virasorovirtual.com.ar/api/index.php').
+  $http.get('http://www.virasorovirtual.com.ar/api/index.php?api=noticias').
           success(function(data) {
-              $scope.greetings = data;
+              $scope.noticias = data;
           });
 
 
-$scope.doRefresh = function() {
-  $http.get('http://www.virasorovirtual.com.ar/api/index.php').
-          success(function(data) {
-              $scope.greetings = data;
-              $scope.$broadcast('scroll.refreshComplete');
-          });    
-    
+  $scope.doRefresh = function() {
+    $http.get('http://www.virasorovirtual.com.ar/api/index.php?api=noticias').
+      success(function(data) {
+        $scope.noticias = data;
+        $scope.$broadcast('scroll.refreshComplete');
+      });
   };
 
-
-
-
 });
+
+
+// .controller('CtrlClasificados', function($scope, $http) {
+
+//   $http.get('http://www.virasorovirtual.com.ar/api/index.php?api=clasificados').
+//           success(function(data) {
+//               $scope.noticias = data;
+//           });
+
+
+//   $scope.doRefresh = function() {
+//     $http.get('http://www.virasorovirtual.com.ar/api/index.php?api=clasificados').
+//       success(function(data) {
+//         $scope.noticias = data;
+//         $scope.$broadcast('scroll.refreshComplete');
+//       });
+//   };
+
+// });
+
+
+
